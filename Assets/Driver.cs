2dd9069f28991bb62b348d5ed51d8d7a5004254d
wekonu <wahjoe.eko.n@gmail.com>
2021-11-01 21:33:00 +0700

    [mod] implement dummy boost and slowdown mechanism + textmeshpro
    - added textmeshpro to make simple game instruction

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float pedalValue  = .01f;
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float pedalValue  = 20f;
+    [SerializeField] float pedalBoost = 30f;
+    [SerializeField] float pedalSlow = 15f;
+
+    float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        //
+        moveSpeed = pedalValue;
     }
 
     // Update is called once per frame
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * pedalValue * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Customer"))
+        {
+            moveSpeed = pedalValue;
+        }
+        else if (other.CompareTag("Package"))
+        {
+            moveSpeed = pedalBoost;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = pedalSlow;   
     }
 }
