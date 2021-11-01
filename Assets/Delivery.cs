eab8886ff7fa0baf893c1584d53c006884f47514
wekonu <wahjoe.eko.n@gmail.com>
2021-11-01 20:48:14 +0700

    [mod] implement dummy package delivery mechanism

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    private bool hasPackage;
+    [SerializeField] private float destroyDelay;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other) 
     {
-        Debug.Log("Duaar!");
+        //
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("Woohoo!");
+        if (other.CompareTag("Package") && !hasPackage)
+        {
+            SpriteRenderer packageRenderer = other.gameObject.GetComponent<SpriteRenderer>();
+
+            Destroy(other.gameObject, destroyDelay);
+            hasPackage = true;
+            
+            if (spriteRenderer != null && packageRenderer != null)
+            {
+                spriteRenderer.color = packageRenderer.color;
+            }
+
+            Debug.Log("Package picked up!");
+        }
+        else if (other.CompareTag("Customer") && hasPackage)
+        {
+            hasPackage = false;
+            
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.color = new Color32(255, 255, 255, 255);
+            }
+
+            Debug.Log("Package delivered!");
+        }
     }
 }
